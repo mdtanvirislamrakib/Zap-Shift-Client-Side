4f6429688a8ffd54585675ef4d58ea516f5138f4
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-06-22 00:21:16 +0600

    firebase setup

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { router } from './Router/Router.jsx';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AuthProvider from './Context/AuthProvider.jsx';
 // ..
 AOS.init();
 
@@ -16,10 +17,10 @@ AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className=''>
     <div className='font-urbanist'>
+      <AuthProvider>
       <RouterProvider router={router} />
-    </div>
+      </AuthProvider>
     </div>
   </StrictMode>,
 )
